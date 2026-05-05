db349c046f277af4f3829a935b427daecf051dad
ValentinaGomez <vgomezgalleguillos@gmail.com>
2026-05-05 16:16:01 -0400

    Ahora sí, carpeta limpia

---
+++ ArbolesSumativa.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// Estructura del nodo 
+struct nodo {
+    int info;
+    struct nodo *izq;
+    struct nodo *der;
+};
+
+typedef struct nodo tNodo;
+typedef tNodo *ABO;
+
+// Reserva memoria para un nuevo nodo e inicializa sus valores
+ABO crearNodo(int valor) {
+    ABO nuevo = (ABO)malloc(sizeof(tNodo));
+    nuevo->info = valor;
+    nuevo->izq = NULL;
+    nuevo->der = NULL;
+    return nuevo;
+}
+// Insertar nuevo valor en arbol binario
+ABO insertarABO(ABO A, int valor) {
+    //Si el árbol está vacío se crea el primer nodo
+    if (A == NULL) {
+        return crearNodo(valor);
+    }
+    //Si el valor es menor que la raiz actual, se inserta en el subarbol izquierdo
+    if (valor < A->info) {
+        A->izq = insertarABO(A->izq, valor);
+    }
+    //Si el valor es mayor, se inserta en el subarbol derecho 
+    else if (valor > A->info) {
+        A->der = insertarABO(A->der, valor);
+    }
+    //Retorna el puntero
+    return A;
+}
+bool estaEnABO (ABO A, int valor){
+    //Si el árbol es nulo el valor no existe
+    if (A==NULL)
+        return false;
+    // Si el valor coincide con la información del nodo actual
+    if (A->info == valor)
+        return true;
+    // Si el valor es menor,se busca en el hijo izquierdo, sino en el derecho.
+    if (valor < A->info)
+        return estaEnABO (A->izq, valor);
+    else
+        return estaEnABO (A->der, valor);
+}
+ABO LeeArchivo(char nombreArchivo[50])
+{
+    FILE *archivo;
+    ABO A;
+    int valor;
+
+    archivo = fopen(nombreArchivo, "r");
+    // verificar si el archivo no existe o no se puede abrir
+    if (archivo == NULL)
+    {
+        printf("No se puede abrir el archivo. Revisa su ubicación.", 163);
+        printf("\n\n");
+        exit(0);
+    }
+    //Inicializar árbol vacío
+    A = NULL;
+    // Ciclo que recorre el archivo
+    while (feof(archivo) == false)
+    {
+        fscanf(archivo, "%i\n", &valor);
+        // insertar el valor si lo encuentra en el ABO
+        if (!estaEnABO(A, valor))
+        {
+            A = insertaABO(A, valor);
+        }
+    }
+
+    fclose(archivo);
+    return A;
+}
